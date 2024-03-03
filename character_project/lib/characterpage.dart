49d0bb655e0479c559d2f7e4ae02320a33711e9d
Jacob Hutchinson <jacob.j.hutchinson.98@gmail.com>
2024-03-03 16:59:02 -0600

    Home and Character pages created

---
+++ character_project/lib/characterpage.dart
@@ -0,0 +1,17 @@
+// character_page.dart
+
+import 'package:flutter/material.dart';
+
+class CharacterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Character Page'),
+      ),
+      body: Center(
+        child: Text('This is the Character Page'),
+      ),
+    );
+  }
+}
--- character_project/lib/main.dart
+++ character_project/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './characterpage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Character Creator Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Home Page'),
     );
   }
 }
@@ -70,56 +71,99 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(200, 200, 200, 1),
+              border: Border.all(
+                color: Colors.black,
+                width: 2.0,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            height: MediaQuery.of(context).size.height / 4,
+            child: Center(
+              child: profilePic(),
             ),
-          ],
-        ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(200, 200, 200, 1),
+              border: Border.all(
+                color: Colors.black,
+                width: 2.0,
+              ),
+            ),
+            height: MediaQuery.of(context).size.height / 4,
+            child: Center(
+              child: profilePic()
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: const Color.fromRGBO(200, 200, 200, 1),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const SizedBox(height: 10),
+                    Text(
+                      "Community Area",
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 24),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  // Widget profilePic() {
+  //   return Container(
+  //     child: Column(children: [
+  //       SizedBox(height: 20),
+  //       CircleAvatar(
+  //         radius: 50,
+  //         backgroundColor: Colors.grey.shade200,
+  //         child: const CircleAvatar(
+  //           radius: 45,
+  //           //backgroundImage: AssetImage('assets/images/plant.jpg'),
+  //         ),
+  //       ),
+  //     ]),
+  //   );
+  // }
+
+  Widget profilePic() {
+  return GestureDetector(
+    onTap: () {
+      // Navigate to another page when the profile pic is clicked
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => CharacterPage()),
+      );
+    },
+    child: Container(
+      child: Column(
+        children: [
+          SizedBox(height: 20),
+          CircleAvatar(
+            radius: 50,
+            backgroundColor: Colors.grey.shade200,
+            child: const CircleAvatar(
+              radius: 45,
+              //backgroundImage: AssetImage('assets/images/plant.jpg'),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
 }
