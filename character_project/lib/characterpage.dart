4dcfe6674a188ca5d71cbaa344544e0b0112287f
Jacob Hutchinson <jacob.j.hutchinson.98@gmail.com>
2024-03-19 20:06:39 -0500

    added additional character page feature prototypes

--- character_project/lib/characterpage.dart
+++ character_project/lib/characterpage.dart
@@ -9,8 +9,65 @@ class CharacterPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Character Name'),
       ),
-      body: Center(
-        child: Text('This is the Character Page'),
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Left side tabs
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.grey[300],
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ListTile(
+                    title: Text('Current Tab'),
+                    onTap: () {
+                      // TODO: change display to selected tab.
+                    },
+                  ),
+                  Divider(),
+                  ListTile(
+                    title: Text('New Tab'),
+                    onTap: () {
+                      // TODO: Create new tab, move lower by 1.
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+          // Central content area
+          Expanded(
+            flex: 3,
+            child: Center(
+              child: Text('This is the Character Page'),
+            ),
+          ),
+
+          // Circle widget (TODO: move to bottom right)
+          Positioned(
+            bottom: 20,
+            right: 20,
+            child: GestureDetector(
+              onTap: () {
+                // TODO: Expand right side menu
+              },
+              child: Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  shape: BoxShape.circle,
+                ),
+                child: Icon(
+                  Icons.menu,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
